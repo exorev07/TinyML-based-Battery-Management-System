5cd7394467db68edd49c9386b0c0d58ae2c9e93f
Ekansh Arohi <ekansharohi1305@gmail.com>
2026-04-08 19:58:34 +0530

    added bottom blur on landing page

--- 02 Dashboard/src/pages/LandingPage.tsx
+++ 02 Dashboard/src/pages/LandingPage.tsx
@@ -6,10 +6,22 @@ import { About } from '../components/landing/About'
 import { Contact } from '../components/landing/Contact'
 import { Footer } from '../components/landing/Footer'
 import ClickSpark from '../components/landing/ClickSpark'
+import GradualBlur from '../components/landing/GradualBlur'
 
 export function LandingPage() {
   return (
     <ClickSpark sparkColor="#b18ddd" sparkSize={10} sparkRadius={30} sparkCount={8} duration={400}>
+      <GradualBlur
+        position="bottom"
+        target="page"
+        height="3rem"
+        strength={3}
+        divCount={5}
+        exponential={true}
+        opacity={0.7}
+        curve="ease-out"
+        style={{ zIndex: 49 }}
+      />
       <Navbar />
       <Hero />
       <Features />
